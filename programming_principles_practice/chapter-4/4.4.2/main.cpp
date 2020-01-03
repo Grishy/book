b7ff4230c1901c81ee83bcbb7c509f9148e7a86a
Grishy <mailgrishy@gmail.com>
2020-01-03 17:58:39 +0300

    Инструкция for

--- programming_principles_practice/chapter-4/4.4.2/main.cpp
+++ programming_principles_practice/chapter-4/4.4.2/main.cpp
@@ -4,11 +4,18 @@
 
 int main()
 {
-    char start = '0';
+    cout << "\t == Инструкция while == \n";
+    char start = 'a';
     char end = 'z';
     while (start <= end)
     {
         cout << start << '\t' << int{start} << "\n";
         ++start;
+    }
+
+    cout << "\t == Инструкция for == \n";
+    for (size_t i = 0; i < 10; i++)
+    {
+        cout << i << '\t' << i * i << "\n";
     }
 }
